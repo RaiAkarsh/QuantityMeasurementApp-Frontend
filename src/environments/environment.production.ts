7e326166a7e340a0073e5939d4ad5f40e5119ffa
Akarsh Rai <akarshrai1405@gmail.com>
2026-04-07 16:51:49 +0530

    [akarsh] Add. Angular

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,5 @@
+// src/environments/environment.production.ts
+export const environment = {
+  production: true,
+  apiUrl: 'http://your-production-gateway-url:8080'  // Change this for production
+};
