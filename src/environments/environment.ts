bc8bbb6d9f4d72624204b0ffcc48e0f445765374
RaiAkarsh <115731369+RaiAkarsh@users.noreply.github.com>
2026-04-16 17:14:39 +0530

    [akarsh] Refactor.Update API URL to production endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'  // API Gateway URL
+  apiUrl: 'https://qty-gateway.onrender.com'  // API Gateway URL
 };
